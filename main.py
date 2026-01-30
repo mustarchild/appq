67c0a6c82372279df5090ec9460e5c667bebc673
mustarchild <159411807+mustarchild@users.noreply.github.com>
2026-01-30 15:03:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.uix.button import Button
 from jnius import autoclass
 
 class BluetoothApp(App):
     def build(self):
+        btn = Button(text="Turn Bluetooth ON")
+        btn.bind(on_press=self.enable_bt)
+        return btn
+
+    def enable_bt(self, instance):
         BluetoothAdapter = autoclass('android.bluetooth.BluetoothAdapter')
+        Intent = autoclass('android.content.Intent')
+
         adapter = BluetoothAdapter.getDefaultAdapter()
-
-        if adapter is None:
-            return Label(text="Bluetooth not supported")
-
-        if not adapter.isEnabled():
-            return Label(text="Bluetooth is OFF")
-
-        return Label(text="Bluetooth is ON")
+        if adapter and not adapter.isEnabled():
+            intent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            self.startActivity(intent)
 
 if __name__ == "__main__":
     BluetoothApp().run()
